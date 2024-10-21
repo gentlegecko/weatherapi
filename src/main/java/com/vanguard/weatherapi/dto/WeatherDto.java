387fa8676a6e16366189d7b56bafe8800f25a835
ethanliang1 <ethan.liang765@hotmail.com>
2024-10-22 02:28:02 +1100

    remove builder

--- src/main/java/com/vanguard/weatherapi/dto/WeatherDto.java
+++ src/main/java/com/vanguard/weatherapi/dto/WeatherDto.java
@@ -2,11 +2,9 @@ package com.vanguard.weatherapi.dto;
 
 import java.util.List;
 
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class WeatherDto {
     private List<WeatherDataDto> weather;
 }
